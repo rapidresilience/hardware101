500167a498f7431c9a6324cc65676ec242144e02
rapidresilience <rapidresilience@icloud.com>
2024-06-09 12:50:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #Change to dark mode
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
-apt-get -y install minicom screen wget
+sudo apt-get update && apt-get -y install minicom screen wget
 
 #Installing Salea Logic
 mkdir ~/tools && cd ~/tools
@@ -12,13 +12,14 @@ chmod +x Logic-2.4.14-linux-x64.AppImage
 mv squashfs-root/ salealogic
 rm ./Logic-2.4.14-linux-x64.AppImage
 
+touch ~/Dekstop/Logic.desktop
 cat > ~/Desktop/Logic.desktop <<EOL
 [Desktop Entry]
 Name=Logic
-Exec=$HOME/tools/salealogic/Logic --no-sandbox
+Exec=~/tools/salealogic/Logic --no-sandbox
 Terminal=false
 Type=Application
-Icon=$HOME/tools/salealogic/Logic.png
+Icon=~/tools/salealogic/Logic.png
 StartupWMClass=Logic
 X-AppImage-Version=2.4.14
 Comment=Debug hardware like a pro
